3b2dfff0e568cc70837e8d804bb708b3129716b1
Cris <cristinamitrica9@gmail.com>
2023-07-03 22:05:37 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductCardComponent } from '../shared/components/molecules/product-card/product-card.component';
+import { HeaderPageComponent } from './layout/header-page/header-page.component';
+import { FooterPageComponent } from './layout/footer-page/footer-page.component';
+import { AllProductsComponent } from './pages/all-products/all-products.component';
+import { CartDetailsComponent } from './pages/cart-details/cart-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductCardComponent,
+    HeaderPageComponent,
+    FooterPageComponent,
+    AllProductsComponent,
+    CartDetailsComponent
   ],
   imports: [
     BrowserModule,
