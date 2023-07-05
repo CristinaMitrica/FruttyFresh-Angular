79ed7c5bfd8fb8b8f8364a149836f90818d7d2ff
Cris <cristinamitrica9@gmail.com>
2023-07-05 18:22:51 +0200

    update

--- src/app/pages/all-products/all-products.component.ts
+++ src/app/pages/all-products/all-products.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/shared/models/products.models';
+import { CartFetchService } from 'src/shared/services/fetchs/cart-fetch.service';
+import { ProductsFetchService } from 'src/shared/services/fetchs/products-fetch.service';
 
 @Component({
   selector: 'app-all-components',
   templateUrl: './all-products.component.html',
   styleUrls: ['./all-products.component.scss']
 })
-export class AllProductsComponent {
+export class AllProductsComponent implements OnInit {
+  public products:Product[] = [];
 
+  constructor (
+    private cartFetchService: CartFetchService,
+    private productsFetchService: ProductsFetchService,
+  ) {}
+
+  ngOnInit():void {
+    this.getProducts();
+  }
+
+  private getProducts():void {
+    this.productsFetchService.getProducts().subscribe((response)=>{
+      this.products = response;
+    });
+  }
 }
--- src/shared/services/fetchs/products-fetch.service.ts
+++ src/shared/services/fetchs/products-fetch.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { Observable, first, map, of } from 'rxjs';
+import { Product } from 'src/shared/models/products.models';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +8,48 @@ import { Injectable } from '@angular/core';
 export class ProductsFetchService {
 
   constructor() { }
+
+  public getProducts():Observable<Product[]> {
+    return of(
+      {
+        data: [
+          {
+              name: 'Pera',
+              price: 5,
+              type: 'fruta'
+          },
+          {
+              name: 'Espárrago',
+              price: 10,
+              type: 'verdura'
+          },
+          {
+              name: 'Cebolla',
+              price: 11,
+              type: 'verdura'
+          },
+          {
+              name: 'Limón',
+              price: 4,
+              type: 'fruta'
+          },
+          {
+              name: 'Plátano',
+              price: 8,
+              type: 'fruta'
+          },
+          {
+              name: 'Zumo',
+              price: 13,
+              type: 'zumo'
+          }
+      ]
+      }
+    ).pipe(
+        map((res) => res.data),
+        first()
+    );
+  }
 }
+
+
